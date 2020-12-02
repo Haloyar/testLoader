e2d8fc00bd3175ca52d6e7e17e5bd6273b972987
Haloyar <yarzoo@bk.ru>
2020-12-02 15:47:45 +0400

    А 100раз

---
+++ src/Loader.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Loader {
+
+    static String mulString(String p, int n) {
+        String result = "";
+        for (int i = 0; i < n; i++)
+            result += p;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(mulString("A", 100));
+    }
+}
+}
